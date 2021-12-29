fdef50aa0a4fb01e92308440f237edb2687bd689
Pedro Madureira <dev@phsolucoesweb.com.br>
2021-12-29 14:49:31 -0300

    initial fixed

---
+++ .config/nvim/init.vim
@@ -0,0 +1,232 @@
+"----------------------/plugins/---------------------------------
+call plug#begin()
+
+"----------/theme
+Plug 'kaicataldo/material.vim'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
+"----------/Icons
+Plug 'ryanoasis/vim-devicons'
+Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
+
+"----------/Syntex highlight
+Plug 'Yggdroot/indentLine'
+Plug 'rrethy/vim-hexokinase', { 'do': 'make hexokinase' } "display colors
+Plug 'HerringtonDarkholme/yats.vim', { 'for': 'typescript' }
+Plug 'posva/vim-vue'
+" Plug 'pangloss/vim-javascript'
+" Plug 'sheerun/vim-polyglot'
+ 
+"----------/Linting
+" Plug 'w0rp/ale'
+
+"----------/search
+Plug 'scrooloose/nerdtree'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'airblade/vim-rooter'
+
+"----------/code completion
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
+"----------/Commenter
+Plug 'scrooloose/nerdcommenter'
+
+"----------/Auto Tags
+Plug 'jiangmiao/auto-pairs'
+
+"----------/git
+" Plug 'Xuyuanp/nerdtree-git-plugin'
+Plug 'airblade/vim-gitgutter'
+"Plug 'tpope/vim-fugitive'
+
+" ----------/python
+" Plug 'python-rope/ropevim'
+
+" ----------/Text Edit
+Plug 'vimwiki/vimwiki'
+
+call plug#end()
+"---------------------/configs/----------------------------
+let mapleader="\<space>"
+set number
+"set relativenumber
+set inccommand=split
+set clipboard=unnamedplus
+
+" TextEdit might fail if hidden is not set.
+set hidden
+
+" --/ Text folding 
+set foldmethod=indent
+set foldlevel=99
+
+" --/indentation
+" set autoindent  ?
+" set smartindent
+" set cindent  ?
+" set smarttab ?
+set shiftwidth=2
+set tabstop=2
+" set softtabstop=2
+" always uses spaces instead of tab characters
+set expandtab
+" save text folding
+autocmd BufWinLeave *.* mkview
+autocmd BufWinEnter *.* silent loadview
+
+"------------------------------/theme/-------------------------
+"--/truecolor configs
+if (has('nvim'))
+	let $NVIM_TUI_ENABLE_TRUE_COLOR = 1
+endif
+
+
+if (has('termguicolors'))
+	set termguicolors
+endif
+
+"--/syntax highlighting
+" syntax enable
+syntax on
+
+set background=dark
+
+" let g:gruvbox_contrast_dark="hard"
+" colorscheme gruvbox
+
+let g:material_theme_style = 'ocean'
+colorscheme material
+
+"--/vim airline theme
+let g:airline_theme='behelit'
+let g:airline#extensions#whitespace#enabled = 0
+
+"--/dev icons
+" Set internal encoding of vim, not needed on neovim, since coc.nvim using some
+" unicode characters in the file autoload/float.vim
+set encoding=UTF-8
+"--/Font
+set guifont=RobotoMono\ Nerd\ Font\ Mono:h12
+
+" ------------------------/nerdtree/---------------------
+
+let g:NERDTreeIgnore = ['^node_modules$']
+" Highlight currently open buffer in NERDTree
+
+"-----------------------/Nerd comments/-----------------------
+filetype plugin on
+let g:NERDSpaceDelims = 1
+let g:NERDDefaultAlign = 'left'
+
+let g:NERDCustomDelimiters = { 'vue': { 'left': '/**','right': '*/', 'leftAlt': '<!--', 'rightAlt': '-->' },
+			\'': { 'left': '---/','right': '', 'leftAlt': '========================/', 'rightAlt': '/======================' } 
+	\}
+
+"OBS: i have commented all CreateMaps commands in /home/phsw/.config/nvim/plugged/nerdcommenter/plugin/nerdcommenter.vim from line 81 to 100, with exeption of:  
+" call s:CreateMaps('nx', 'Invert',     'Invert', 'c')  <-- I did this because i want to avoid keybind delay
+" call s:CreateMaps('n',  'AltDelims',  'Switch Delimiters', 'C')
+" This this corresponds to NERDCommenterAltDelims and NERDCommenterInvert. 
+"
+" ---------------------------/Indent Line
+let g:indentLine_enabled = 1
+"map <C-k>i :IdentLinesToggle
+
+let g:indentLine_fileTypeExclude = ['json', 'markdown', 'md']
+
+let s:box_drawings_light_vertical='│'
+
+" let g:indentLine_char=s:box_drawings_light_vertical
+
+
+" ---------------------------/snippets
+" Use <C-k> for jump to previous placeholder, it's default of coc.nvim
+let g:coc_snippet_next = '<C-j>'
+let g:coc_snippet_prev = '<C-k>'
+
+
+" ---------------------------/Coc-prettier
+
+" nnoremap <leader>F :CocCommand prettier.formatFile
+vmap <leader>f :CocCommand prettier.formatFile
+nmap <leader>f :CocCommand prettier.formatFile
+command! -nargs=0 Prettier :CocCommand prettier.formatFile
+
+
+" ---------------------------/Hexodinase (colors)
+
+let g:Hexokinase_ftAutoload = ['css', 'less']
+
+" ---------------------------/Python import
+" let g:ropevim_autoimport_modules = ["os", "shutil", "rolepermissions", "django"]
+" let g:pymode_rope_autoimport_modules = ["os", "shutil"]
+" let g:ropevim_enable_autoimport = 1
+" let g:ropevim_guess_project = 1
+" let g:ropevim_guess_project = "$VIRTUAL_ENV/../.ropeproject/config.py"
+"
+" ---------------------------/Vimwiki
+set nocompatible
+let g:vimwiki_list = [{'path': '~/Documents/vimwiki/'}]
+
+" let g:vimwiki_list = [{'path': '~/my_site/', 'path_html': '~/public_html/', 'syntax': 'markdown', 'ext': '.md}]
+
+"----------------------/Remaps/----------------------------
+"ex mode is useless
+map Q :q! <Cr>
+map q :q <Cr>
+
+"Case insensitive search
+nnoremap <leader>/ /\c
+nnoremap <leader>? ?\c
+"xnoremap = visual mode
+"nnoremap = normal mode
+nnoremap J }
+xnoremap J }
+nnoremap K {
+xnoremap K {
+
+cnoremap <C-F> <Right>
+cnoremap <C-B> <Left>
+" cnoremap <Esc>b <S-Left>
+" cnoremap <Esc>f <S-Right>
+
+" nnoremap H B
+" nnoremap L W
+nnoremap <leader>d "_d
+xnoremap <leader>d "_d
+nnoremap <leader>D "_D
+nnoremap <leader>o :NERDTreeToggle <Cr>
+" nnoremap <leader>w :w! <Cr>
+map s :w! <Cr>
+" nnoremap <leader>q :q <Cr>
+" nnoremap <leader>Q :q! <Cr>
+nnoremap <leader>n :bnext<esc>
+nnoremap <leader>p :bprevious<esc>
+nnoremap <leader>x :bd<esc>
+nnoremap <leader>X :bd!<esc>
+nnoremap <leader>ec :vsplit ~/.config/nvim/init.vim<cr>
+nnoremap <leader>lc :source ~/.config/nvim/init.vim<cr>
+nnoremap <leader>es :CocCommand snippets.openSnippetFiles
+nnoremap <leader>ls :CocList snippets <cr>
+nnoremap <leader>m za
+
+inoremap <C-j> <C-n>
+inoremap <C-k> <C-p>
+
+" ---/ Actions
+
+map <F12> :PlugInstall <CR>
+map <F1> :! firefox "%"<CR>
+map <F2> :! mng runserver<CR>
+map <F6> :setlocal spell! spelllang=en_us<CR>
+" grant sudo permissions and save
+" map <F3> :w !sudo tee %
+
+" ---/test (M => alt)
+nnoremap <M-Right> :bn<cr>
+nnoremap <M-Left> :bp<cr>
+
+"---------------------/Source/--------------------
+source ~/.config/nvim/fzf.nvimrc
+source ~/.config/nvim/coc.nvimrc
---
+++ .config/qtile/config.py
@@ -0,0 +1,318 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import List  # noqa: F401
+
+from libqtile import bar, layout, widget, extension
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from libqtile import qtile
+
+mod = "mod4"
+terminal = guess_terminal()
+
+def fix_cli_app(app):
+    '''Quick fix of github.com/qtile/qtile/issues/2167 bug'''
+    fix_environment = 'export -n LINES; export -n COLUMNS; sleep 0.01 &&'
+    return f'alacritty -t "{app}" -e sh -c "{fix_environment} {app}"'
+
+keys = [
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    #  Key([mod], "space", lazy.layout.next(),  <---- (useless)
+        #  desc="Move window focus to other window"),
+
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    #  Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+
+    #  Key([mod, "shift"], "Return", lazy.layout.toggle_split(),  <---- ?
+        #  desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
+
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+
+    #------/ SCREENSHOTS
+    Key([], "Print", lazy.spawn("scrot 'ArcoLinux-%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f ~/Pictures'")),
+    Key([mod], "Print", lazy.spawn("scrot -s  'ArcoLinux-%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f ~/Pictures'")),
+
+    #------/ Key apps
+    Key([mod, "control"], "f", lazy.spawn("ranger-fix")),
+    Key([mod, "control"], "a", lazy.spawn(terminal + " -e alsamixer")),
+    Key([mod], "b", lazy.spawn("brave")),
+    Key([mod], "e", lazy.spawn(fix_cli_app("neomutt"))),
+    Key([mod], "m", lazy.spawn(terminal + " -e mocp")),
+
+    #-----/ Scripts
+    #  Key([mod, "control"], "d", lazy.run_extension(extension.DmenuRun(
+            #  dmenu_prompt="$",
+            #  dmenu_bottom=False,
+            #  command='echo -e "first second third"',
+            #  dmenu_command="dmenu"
+            #  dmenu_lines=42
+        #  ))),
+    Key([mod, "control"], "d", lazy.spawn("dmenu_phsw")),
+    Key([mod, "control"], "p", lazy.spawn("passmenu -h 25 -fn 'sans-10'")),
+    Key([mod, "control"], "9", lazy.spawn("sudo killall wpa_supplicant")),
+    Key([mod, "control"], "8", lazy.spawn("sudo systemctl start wpa_supplicant@wlp0s20f3.service")),
+    Key([mod, "mod1"], "Escape", lazy.spawn("sudo shutdown now")),
+    Key([mod, "lock"], "Escape", lazy.spawn("reboot")),
+    Key([mod, "control"], "Escape", lazy.spawn("xlock -delay 10000 -mode random")),
+    Key([mod], "1", lazy.spawn("sudo xrandr --output eDP1  --brightness 0.8")),
+    Key([mod], "2", lazy.spawn("sudo xrandr --output eDP1  --brightness 0.7")),
+    Key([mod], "3", lazy.spawn("sudo xrandr --output eDP1  --brightness 0.6")),
+    # Documents
+    #  Key([mod], "t", lazy.spawn(terminal + " -e nvim /home/phsw/Documents/ideals/todo")),  <--- old vim command
+    Key([mod], "t", lazy.spawn(fix_cli_app("nvim /home/phsw/Documents/vimwiki/Todo.wiki"))),
+    Key([mod], "n", lazy.spawn(fix_cli_app("nvim /tmp/newfile"))),
+    Key([mod], "w", lazy.spawn(fix_cli_app("nvim /home/phsw/Documents/vimwiki/index.wiki"))),
+    # configs
+    Key([mod, "control","mod1"], "0", lazy.spawn(fix_cli_app("sudo nvim /home/phsw/.local/share/qtile/qtile.log"))),
+    Key([mod, "control"], "0", lazy.spawn(fix_cli_app("nvim /home/phsw/.config/qtile/config.py"))),
+    Key([mod, "control"], "n", lazy.spawn(fix_cli_app("nvim /home/phsw/.config/nvim/init.vim"))),
+    Key([mod, "control"], "o", lazy.spawn(fix_cli_app("nvim /home/phsw/.config/ranger/rc.conf"))),
+]
+
+groups = [Group(i) for i in "asdfuiop"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+
+layout_theme = {"border_width": 1,
+                "margin": 0,
+                "border_focus":"#71a3ad",
+                "border_normal": "1D2330"
+                }
+
+layouts = [
+    #  layout.Columns(border_focus_stack='#d75f5f', **layout_theme),
+    layout.Columns(**layout_theme),
+    layout.Max(**layout_theme),
+    # Try more layouts by unleashing below layouts.
+    #  layout.Stack(num_stacks=2),
+    #  layout.Bsp(),
+    #  layout.Matrix(),
+    #  layout.MonadTall(),
+    #  layout.MonadWide(),
+    #  layout.RatioTile(),
+    #  layout.Tile(),
+    #  layout.TreeTab(),
+    #  layout.VerticalTile(),
+    #  layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=2,
+    background="#000000"
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        #  bottom=bar.Bar(
+        top=bar.Bar(
+            [
+                widget.Sep(
+                       linewidth = 0,
+                       padding = 8,
+                       foreground = '#ffffff',
+                       background = '#000000'
+                       ),
+
+                widget.Image(
+                    filename = "~/.config/qtile/icons/python-icon.png",
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("unimatrix-fix")},
+                ),
+                widget.GroupBox(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(),
+
+                widget.Prompt(),
+
+                widget.Maildir(maildir_path="~/.local/share/mail/contato@phsolucoesweb.com.br", 
+                    sub_folders=[{'label': 'Inbox', 'path': 'INBOX'}],
+                    #  sub_folders=[{'label': 'Home mail', 'path': 'INBOX'}, {'label': 'Home junk', 'path': 'spam'}],
+                    hide_when_empty=True,
+                    update_interval=10
+                    ),
+
+                widget.Sep(
+                       linewidth = 0,
+                       padding = 9,
+                       foreground = '#ffffff',
+                       background = '#000000'
+                       ),
+
+                widget.Image(
+                   filename = "~/.config/qtile/icons/music.png",
+                   mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + " -e mocp")}
+                ), 
+
+                widget.TextBox(text = '   |  ', foreground = '#ffffff', padding = 0, fontsize = 26),  # <---------------
+
+                widget.Image(
+                   filename = "~/.config/qtile/icons/sound_icon.png",
+                   mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("alacritty -e alsamixer")}
+                ),
+
+                widget.TextBox(text = '   | ', foreground = '#ffffff', padding = 0, fontsize = 26),  # <---------------
+
+                widget.Sep(
+                       linewidth = 0,
+                       padding = 8,
+                       foreground = '#ffffff',
+                       background = '#000000'
+                       ),
+
+                widget.TextBox(
+                       text = '⚡  ',
+                       #  text = '⌛',
+                       #  background = "#282c34",
+                       foreground = '#ffffff',
+                       padding = 0,
+                       fontsize = 22
+                       ),
+                widget.Battery(format='{percent:2.0%}', foreground='#ffffff'),
+
+                widget.TextBox(text = '  |  ', foreground = '#ffffff', padding = 0, fontsize = 26),  # <---------------
+
+                widget.Image(
+                       filename = "~/.config/qtile/icons/calendar.png",
+                       ),
+                widget.Clock(format='%A, %B %d  ', foreground='#ffffff'),
+
+                widget.TextBox(text = '  |  ', foreground = '#ffffff', padding = 0, fontsize = 26),  # <---------------
+
+                widget.TextBox(
+                       text = '⌚ ',
+                       #  text = '⌛',
+                       #  background = "#282c34",
+                       foreground = '#ffffff',
+                       padding = 0,
+                       fontsize = 22
+                       ),
+                widget.Clock(format='%I:%M    ', foreground='#ffffff'),
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
