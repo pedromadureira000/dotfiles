c28e2346592bd8251e2463b2838e19b430b7a23e
Pedro Madureira <ph.websolucoes@gmail.com>
2024-09-26 15:10:07 -0300

    configs

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>S', builtin.find_files, {})
+vim.keymap.set('n', '<leader>s', builtin.find_files, {})
 --vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>s', builtin.git_files, {})
+vim.keymap.set('n', '<leader>S', builtin.git_files, {})
 vim.keymap.set('n', '<leader>g', function()
   builtin.grep_string({search = vim.fn.input("Grep > ") });
 end)
