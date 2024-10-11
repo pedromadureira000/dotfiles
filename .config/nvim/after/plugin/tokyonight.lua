5c6a1ab960f9ff7f37b4bf10b63d28626408bff1
Pedro Madureira <ph.websolucoes@gmail.com>
2024-10-11 15:54:03 -0300

    colors are good now nvim md headers

--- .config/nvim/after/plugin/tokyonight.lua
+++ .config/nvim/after/plugin/tokyonight.lua
@@ -12,23 +12,23 @@ require("tokyonight").setup({
         colors.bg = "#000000"  -- Changed from red to black for better visibility
     end,
     on_highlights = function(hl, c)
-        -- H1 - Dark Red (Less Muted)
+        -- H1 - Dark Red
         hl["RenderMarkdownH1Bg"] = {bg = "#5a2626", bold = true }
-        hl["@markup.heading.1.markdown"] = {fg = "#cc4444", bold = true }
-        -- H2 - Dark Yellow (Less Muted)
+        hl["@markup.heading.1.markdown"] = {fg = "#bd0000", bold = true }
+        -- H2 - Dark Yellow
         hl["RenderMarkdownH2Bg"] = {bg = "#5c5c26", bold = true}
-        hl["@markup.heading.2.markdown"] = {fg = "#cccc00", bold = true}
-        -- H3 - Dark Green (Less Muted)
+        hl["@markup.heading.2.markdown"] = {fg = "#c2bf00", bold = true}
+        -- H3 - Dark Green
         hl["RenderMarkdownH3Bg"] = {bg = "#265c39", bold = true}
-        hl["@markup.heading.3.markdown"] = {fg = "#44cc77", bold = true}
-        -- H4 - Dark Cyan (Less Muted)
+        hl["@markup.heading.3.markdown"] = {fg = "#00bd0a", bold = true}
+        -- H4 - Dark Cyan
         hl["RenderMarkdownH4Bg"] = {bg = "#265c5c", bold = true}
-        hl["@markup.heading.4.markdown"] = {fg = "#44cccc", bold = true}
-        -- H5 - Dark Blue (Less Muted)
+        hl["@markup.heading.4.markdown"] = {fg = "#13cfcf", bold = true}
+        -- H5 - Dark Blue
         hl["RenderMarkdownH5Bg"] = {bg = "#26375c", bold = true}
-        hl["@markup.heading.5.markdown"] = {fg = "#4477cc", bold = true}
-        -- H6 - Dark Purple (Less Muted)
+        hl["@markup.heading.5.markdown"] = {fg = "#4775ff", bold = true}
+        -- H6 - Dark Purple
         hl["RenderMarkdownH6Bg"] = {bg = "#5c265c", bold = true}
-        hl["@markup.heading.6.markdown"] = {fg = "#cc44cc", bold = true}
+        hl["@markup.heading.6.markdown"] = {fg = "#c714c7", bold = true}
     end
 })
