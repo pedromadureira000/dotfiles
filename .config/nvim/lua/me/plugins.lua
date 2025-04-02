827855d27beea57d6898178faa6bee3e022cbc8d
Pedro Madureira <ph.websolucoes@gmail.com>
2025-04-02 15:30:11 -0300

    commentaries removed

--- .config/nvim/lua/me/plugins.lua
+++ .config/nvim/lua/me/plugins.lua
@@ -59,21 +59,11 @@ local plugins = {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {
-        -- heading = {
-        -- },
         bullet = {
-            -- Turn on / off list bullet rendering
             enabled = false,
-            -- Replaces '-'|'+'|'*' of 'list_item'
-            -- How deeply nested the list is determines the 'level'
-            -- The 'level' is used to index into the list using a cycle
-            -- If the item is a 'checkbox' a conceal is used to hide the bullet instead
             icons = { '●', '○', '◆', '◇' },
-            -- Padding to add to the left of bullet point
             left_pad = 0,
-            -- Padding to add to the right of bullet point
             right_pad = 0,
-            -- Highlight for the bullet icon
             highlight = 'RenderMarkdownBullet',
         },
     },
